51d38f5a72af198bd64949edb0ae4a0ecd4de65a
patrickNgare <patrickngare@gmail.com>
2018-02-22 20:19:20 +0300

    added views

--- portifolioPro/porti/views.py
+++ portifolioPro/porti/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Portifolio, Tag
+from django.db.models.base import ObjectDoesNotExist
 
-def welcome(request):
-    return render(request,'welcome.html')
+
+def projects(request):
+    
+    projo=Portifolio.all_projects()  
+
+
+    return render(request,'index.html',{"projo":projo})
+
+
+
+def description(request,id):
+    
+    try:
+   
+        project=Portifolio.objects.get(id=id)
+    except Portifolio.DoesNotExist:
+        raise Http404()  
+
+    return render(request,'all-news/portofolio.html',{"project": project})
