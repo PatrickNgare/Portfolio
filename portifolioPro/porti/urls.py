f9bf762fc38065881823b5add2dd7c31f6aa6780
patrickNgare <patrickngare@gmail.com>
2018-02-22 20:18:53 +0300

    added urls

--- portifolioPro/porti/urls.py
+++ portifolioPro/porti/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns=[
-        url('^$',views.welcome, name='welcome'),
+        url('^$',views.projects, name='projects'),
+        url('^details/(\d+)',views.description,name='details')
 
 ]
+
+if settings.DEBUG:
+        urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
