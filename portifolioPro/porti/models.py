5c03a8484739226802ca4f9705e55fff3f94cab5
patrickNgare <patrickngare@gmail.com>
2018-02-22 20:17:59 +0300

    added models

--- portifolioPro/porti/models.py
+++ portifolioPro/porti/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.validators import URLValidator
 
-# Create your models here.
+
+
+
+class Tag(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+        
+class Portifolio(models.Model):
+    title=models.CharField(max_length=70)
+    details=models.TextField()
+    giturl=models.TextField(validators=[URLValidator()])
+    deployedurl=models.TextField(validators=[URLValidator()])
+    porttags=models.ManyToManyField(Tag)
+    port_image=models.ImageField(upload_to= 'portifolio/', null=True,blank=True)
+
+
+    @classmethod
+    def all_projects(cls):
+        
+        projects=cls.objects.all()
+        return projects
+    
+
+    # @classmethod
+    # def single_project(cls):
+    #     projects=cls.objects.get(id)
+    #     return projects
+
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
