9b4ab014bf72fb85da8b348394c0cda43fa73adf
patrickNgare <patrickngare@gmail.com>
2018-02-22 20:18:28 +0300

    added tests for portfolio tests

--- portifolioPro/porti/tests.py
+++ portifolioPro/porti/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Tag,Portifolio
 
-# Create your tests here.
+
+class PortifolioTestClass(TestCase):
+    
+
+
+    def setUp(self):
+        self.moveisIP=Portifolio(title='movies libary app',details='module This class inherits from the TestCase class. We then create a setUp method that allows',giturl='https://github.com/PatrickNgare/landingPage',deployedurl='www.example.com')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.moveisIP,Portifolio))
+
+def test_save_method(self):
+    self.moveisIP.save_editor()
+    portifolio=Portifolio.objects.all()
+    self.assertTrue(len(portifolio)>0)
