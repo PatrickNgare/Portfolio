79321e7afc537ead10c9eae6004162d48029e6e7
patrickNgare <patrickngare@gmail.com>
2018-02-22 20:23:08 +0300

    added admin page

--- portifolioPro/porti/admin.py
+++ portifolioPro/porti/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tag,Portifolio
+
+
+class PortifolioAdmin(admin.ModelAdmin):
+    filter_horizontal=('porttags',)
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Portifolio,PortifolioAdmin)
